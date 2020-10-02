72e89ba0a065094da7bc2a3869ea2f47ae7f0ab6
Andrea <andry.tino@gmail.com>
2020-10-02 11:05:31 +0200

    Add unit tests for SortingJob

---
+++ maersk2020.dotnet/WebIntSorter/WebIntSorter.UnitTests/SortingJobUnitTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Challenge.WebIntSorter.UnitTests
+{
+    [TestClass]
+    public class SortingJobUnitTest
+    {
+        [TestMethod]
+        public void WhenCreatedThenNewGuidIsAssigned()
+        {
+            var job = new SortingJob();
+            Assert.IsNotNull(job.Id, "A new id should have been assigned");
+        }
+
+        [TestMethod]
+        public void IdsAreUnique()
+        {
+            var job1 = new SortingJob();
+            var job2 = new SortingJob();
+            Assert.AreNotEqual(job1.Id, job2.Id, "Ids should be different");
+        }
+
+        [TestMethod]
+        public void WhenCreatedThenTimestampIsAssigned()
+        {
+            var job = new SortingJob();
+            Assert.IsNotNull(job.Timestamp, "A new timestamp should have been assigned");
+        }
+    }
+}
